227e4b62979fcdb578d4e55a86fcbb10fdc89fc9
Md Aminul Islam <mr.aminul118@gmail.com>
2025-01-07 07:56:47 +0600

    Top rated doctors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -8,11 +9,31 @@ const port = process.env.PORT || 5000;
 
 /**
  * -----------------------------------
- * ! Middleware
+ * ! Middleware uses
  * -----------------------------------
  */
 app.use(cors());
 app.use(express.json());
+
+/**
+ * -----------------------------------
+ * ! Customs Middleware
+ * -----------------------------------
+ */
+const verifyToken = (req, res, next) => {
+  // console.log("inside verify token", req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 /**
  * -----------------------------------
@@ -44,6 +65,77 @@ async function run() {
     const db = client.db("easyDoc");
     const doctorCollections = db.collection("doctors");
     const appointmentCollections = db.collection("appointments ");
+    const userCollections = db.collection("users");
+    /**
+     * -----------------------------------
+     * ! JWT security related api
+     * -----------------------------------
+     */
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
+    });
+
+    /**
+     * -----------------------------------
+     * ! User related api
+     * -----------------------------------
+     */
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollections.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", verifyToken, async (req, res) => {
+      // console.log(req.decoded);
+      const result = await userCollections.find().toArray();
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    /**
+     * -----------------------------------
+     * ! Admin related api
+     * -----------------------------------
+     */
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollections.updateOne(query, update);
+      res.send(result);
+    });
+
     /**
      * -----------------------------------
      * ! Doctor related api
@@ -52,12 +144,40 @@ async function run() {
 
     app.post("/doctors", async (req, res) => {
       const data = req.body;
+      const email = { email: data.email };
+
+      const query = await doctorCollections.findOne(email);
+      if (query.email === email) {
+        return res.status(403).send({ message: "user already exits" });
+      }
+
       const result = await doctorCollections.insertOne(data);
       res.send(result);
     });
-    
+
     app.get("/doctors", async (req, res) => {
       const result = await doctorCollections.find().toArray();
+      res.send(result);
+    });
+    app.get("/top-rated-doctors", async (req, res) => {
+      const aggregate = [
+        {
+          $sort: { rating: -1 },
+        },
+        {
+          $limit: 8,
+        },
+        {
+          $project: {
+            doctorName: 1,
+            specialization: 1,
+            experience: 1,
+            rating: 1,
+            image: 1,
+          },
+        },
+      ];
+      const result = await doctorCollections.aggregate(aggregate).toArray();
       res.send(result);
     });
     app.get("/doctors/:id", async (req, res) => {
